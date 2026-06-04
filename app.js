ebd30354b7180d786a04baea6c87cce982b59189
yugi78 <60105855+yugi78@users.noreply.github.com>
2026-06-04 21:57:46 +0200

    fov mob 2 desk 1.5

--- app.js
+++ app.js
@@ -440,7 +440,7 @@ const createScene = async function () {
                 groundCam.angularSensibility = 1000; // Sensibilité tactile réactive
 
 		// 📱 FOV MOBILE : On l'ouvre à 1.1 (~63° vertical) pour un effet grand angle
-                groundCam.fov = 3;
+                groundCam.fov = 2;
                 
                 // On force le module Souris à écouter vos doigts sur l'écran
                 if (groundCam.inputs.attached.mouse) {
